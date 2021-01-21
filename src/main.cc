26fea008e18a581300e19e110d2e6327008f04ab
Rufus Wong <rcywongaa@gmail.com>
2021-01-21 16:40:14 +0800

    Use ros time as timestamp to allow synchronization

--- src/main.cc
+++ src/main.cc
@@ -91,7 +91,7 @@ public:
   void lidarCallback(boost::shared_ptr<PPointCloud> cld, double timestamp, hesai_lidar::PandarScanPtr scan) // the timestamp from first point cloud of cld
   {
     if(m_sPublishType == "both" || m_sPublishType == "points"){
-      pcl_conversions::toPCL(ros::Time(timestamp), cld->header.stamp);
+      pcl_conversions::toPCL(ros::Time::now(), cld->header.stamp);
       cld->header.frame_id = frame_id;
       sensor_msgs::PointCloud2 output;
       pcl::toROSMsg(*cld, output);
